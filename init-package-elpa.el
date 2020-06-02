56291c72ceb4e0e085fda471463dca2e693e73c7
fangxiao <fangxiao0313@hotmail.com>
2020-06-02 17:13:19 +0800

    Add company-tabnine.

--- init-package-elpa.el
+++ init-package-elpa.el
@@ -711,3 +711,10 @@
 
 (provide 'init-package-elpa)
 ;;; init-package-elpa.el ends here
+(require 'company-tabnine)
+(add-to-list 'company-backends #'company-tabnine)
+;; Trigger completion immediately.
+(setq company-idle-delay 0)
+
+;; Number the candidates (use M-1, M-2 etc to select completions).
+(setq company-show-numbers t)
